4720dc331ee895b3d6d374b9863ca254569d6c6d
9 <a>
2026-06-02 11:34:20 +0900

    poop

---
+++ src/ui.h
@@ -0,0 +1,12 @@
+#ifndef UI_H
+#define UI_H
+
+int aisatu_ui();
+int kyouka_ui();
+int kesseki_ui();
+int tanni_ui();
+int jumyou_ui(char kyouka[],int kesseki,int tanni);
+int kakuninn_ui();
+int END_ui();
+
+#endif
