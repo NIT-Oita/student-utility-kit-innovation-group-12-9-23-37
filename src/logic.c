e87830119685cbceb85277f14edfac967de3653d
s2523 <s2523@oita.kosen-ac.jp>
2026-06-02 11:41:42 +0900

    logicの試作

---
+++ src/logic.c
@@ -0,0 +1,18 @@
+#include "logic.h"
+#include <stdio.h>
+
+int allkake;
+
+double jyumyou(char a[],int b,int c){
+    int stkake;
+
+    stkake=outstrage(a);
+
+    allkake=stkake+b;
+
+    return 3.5*c-allkake;
+}
+
+int instrage(char a[]){
+    return allkake;
+}
---
+++ src/logic.h
@@ -0,0 +1,8 @@
+#ifndef LOGIC_H
+#define LOGIC_H
+
+int instorage(char name[]);
+int outstorage(char name[]);
+double jumyou(char name[],int kake,int tanni);
+
+#endif
